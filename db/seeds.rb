3a05b101c4d886e80eb489cd53497c447f28758f
LindaHendricks <linda.timoh@gmail.com>
2021-06-30 11:03:29 -0400

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,11 @@ dio = Fashiongirl.create(name: 'Dio', age: 20, location: 'Paris', portfolio: 'ht
 stella = Fashiongirl.create(name: 'Stella', age: 22, location: 'Paris', portfolio: 'https://linda-hendricks.com/')
 melissa = Fashiongirl.create(name: 'Melissa', age: 26, location: 'Paris', portfolio: 'https://linda-hendricks.com/')
 
-sami = Photographer.create(name: 'Sami', location:'Paris', portfolio: "https://i.pinimg.com/564x/f8/1d/91/f81d9195c9fcad824d13c1395674278f.jpg", popularity: 5)
-ange = Photographer.create(name: 'Ange', location:'Paris',portfolio: "https://i.pinimg.com/564x/1f/ce/93/1fce9315f5c0823456ac123cebe56667.jpg", popularity: 4)
-nicolas = Photographer.create(name: 'Nicolas', location:'Paris', portfolio: "https://i.pinimg.com/564x/1b/be/bd/1bbebd1f4f6f0eb81b4f0e6eff5025b5.jpg", popularity: 1)
-fred = Photographer.create(name: 'Fred', location:'Paris', portfolio: "https://i.pinimg.com/564x/26/25/30/2625302c692dd5250ecf713e5723c5ee.jpg", popularity: 2)
-alan = Photographer.create(name: 'Alan', location:'Paris', portfolio: "https://i.pinimg.com/564x/4e/b7/d2/4eb7d20a70f294132658571384e66200.jpg", popularity: 3)
+sami = Photographer.create(name: 'Sami', location:'Paris', portfolio: 'https://cdn-images-1.medium.com/max/1200/1*ev8fYgYjMrRzDxafi6lWAw.jpeg', popularity: 5)
+ange = Photographer.create(name: 'Ange', location:'Paris',portfolio: 'www.portfoliotocome.com', popularity: 4)
+nicolas = Photographer.create(name: 'Nicolas', location:'Paris', portfolio: 'www.portfoliotocome.com', popularity: 1)
+fred = Photographer.create(name: 'Fred', location:'Paris', portfolio: 'www.portfoliotocome.com', popularity: 2)
+alan = Photographer.create(name: 'Alan', location:'Paris', portfolio: 'www.portfoliotocome.com', popularity: 3)
 
 
 Shooting.create(date: "12 Novembre", location:"Paris", fashiongirl_id: alisson.id, photographer_id: sami.id)
